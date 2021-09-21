cdc6793152ab8aa55354fb158e8d6ff400e40ad4
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-21 20:25:02 +0530

    ZEENAT: added main method

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DAY3_Line_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UC1_CalculateLength.Line1Length();
+            UC2_CheckEqualLength.CheckEqualLength();
+            UC3_ComparingTwoLenghts.CompareTwoLengths();
+            Console.WriteLine();
+
+        }
+    }
+}
